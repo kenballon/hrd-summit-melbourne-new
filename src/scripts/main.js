1e90b723ddfe2fed23289f9d9bd17c0e96a34445
kenballon <49665487+kenballon@users.noreply.github.com>
2023-06-07 22:02:10 +0800

    Mobile Hamburger Menu Animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@ document.addEventListener("readystatechange", (e) => {
   if (e.target.readyState === "complete") {
     // call function here below
     callStickyNavBarFunction();
+    callOpenMobileMenu();
   }
 });
 
@@ -19,7 +20,34 @@ const callStickyNavBarFunction = () => {
   });
 };
 
-const callHamburgerMenuReveal = () => {
+const callOpenMobileMenu = () => {
   // TODO:
   /* Event Listener for the button to hide and show menu */
+  const mobileMenuBtn = document.getElementById("openCloseMobileNav");
+  const revealMobileMenuNavLinks = document.querySelector(
+    ".navigation-links-parent"
+  );
+  const bodyFixedScroll = document.querySelector("body");
+  const addClassToFadeOut = document.querySelector(".navigation-links-parent");
+
+  mobileMenuBtn.addEventListener("click", (e) => {
+    if (mobileMenuBtn.getAttribute("aria-label") == "Open Navigation") {
+      mobileMenuBtn.setAttribute("aria-label", "Close Navigation");
+      mobileMenuBtn.classList.add("nav-toggle--close");
+      revealMobileMenuNavLinks.classList.add("show-mobile-menu");
+      bodyFixedScroll.classList.add("fixed-modal-reveal");
+    } else if (mobileMenuBtn.getAttribute("aria-label") == "Close Navigation") {
+      mobileMenuBtn.setAttribute("aria-label", "Open Navigation");
+      mobileMenuBtn.classList.remove("nav-toggle--close");
+
+      // add class before closing for fadeout animation of mobile nav
+      addClassToFadeOut.classList.add("fade-out-animate");
+
+      setTimeout(() => {
+        bodyFixedScroll.classList.remove("fixed-modal-reveal");
+        revealMobileMenuNavLinks.classList.remove("show-mobile-menu");
+        addClassToFadeOut.classList.remove("fade-out-animate");
+      }, 150);
+    }
+  });
 };
